50cde912fba81e3d13b0b067aa8404910a00f40a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-12-02 11:10:54 -0800

    created a new migration to add an image to article schema, also practiced removing a column

---
+++ db/migrate/20191202190352_add_img_to_article.rb
@@ -0,0 +1,5 @@
+class AddImgToArticle < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :img, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_213008) do
+ActiveRecord::Schema.define(version: 2019_12_02_190940) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "comments", force: :cascade do |t|
